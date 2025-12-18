c7867ab6698d1aec25da64edb4f544fa5d4bae64
Nathanpoonst0116 <poonshingtsannathan@gmail.com>
2025-12-18 21:39:50 +0800

    add group name and declaration
    add group name and declaration

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,24 @@
+/** 
+I declare that the lab work here submitted is original
+except for source material explicitly acknowledged, and that the same or closely related material has not been
+previously submitted for another course.
+I also acknowledge that I am aware of University policy and
+regulations on honesty in academic work, and of the disciplinary
+guidelines and procedures applicable to breaches of such
+policy and regulations, as contained in the website. University Guideline on Academic Honesty:
+https://www.cuhk.edu.hk/policy/academichonesty/
+
+Group 12 
+Group member: 
+POON Shing Tsan				1155193858 
+WONG San Ki Sunny			1155193541 
+KUMARBEK UULU Chyngyz		1155190408 
+LAM Ka Shing				1155194077 
+YAM Tin Lam Lucas			1155234981 
+Class/Section : CSCI2720
+Date : 18-12-2025 
+**/
+
 // Cultural Events App - Main JavaScript
 
 // Global variables
@@ -499,7 +520,25 @@ function showVenueDetail(venueId) {
         });
 }
 
-function displayVenueDetail(venue) {
+async function getVenueAddress(venue) {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${venue.latitude}&lon=${venue.longitude}&zoom=18&addressdetails=1&accept-language=en`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data && data.display_name) {
+            return data.display_name;
+        } else {
+            return "NMM";
+        }
+    } catch (error) {
+        console.error("Error during reverse geocoding:", error);
+        return "NMM";
+    }
+}
+
+async function displayVenueDetail(venue) {
     const contentDiv = document.getElementById('venueDetailContent');
     
     // Check Venue Like Status
@@ -507,6 +546,7 @@ function displayVenueDetail(venue) {
     const isVenueLiked = userId && venue.likes && venue.likes.includes(userId);
     const venueLikeClass = isVenueLiked ? 'liked' : '';
     const venueLikeCount = venue.likes ? venue.likes.length : 0;
+    let venueAdress = await getVenueAddress(venue);
 
     contentDiv.innerHTML = `
         <div class="venue-header">
@@ -528,8 +568,8 @@ function displayVenueDetail(venue) {
             <div class="venue-section">
                 <h3>Venue Information</h3>
                 <div id="venueMap" style="height: 300px; margin-bottom: 1rem;"></div>
-                <p><strong>Address:</strong> ${venue.address || 'N/A'}</p>
-                <p><strong>Description:</strong> ${venue.description || 'N/A'}</p>
+                <p><strong>Region:</strong> ${venue.region || 'N/A'}</p>
+                <p><strong>Address:</strong> ${venueAdress || 'N/A'}</p>
             </div>
             
             <div class="venue-section">
